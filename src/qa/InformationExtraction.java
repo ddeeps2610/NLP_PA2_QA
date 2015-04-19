50fda4907e6f1f04f835a64f7f536b9ed37a8106
manvi90 <manvimalik09@gmail.com>
2015-04-18 22:37:11 -0400

    information extraction updated

--- src/qa/InformationExtraction.java
+++ src/qa/InformationExtraction.java
@@ -3,17 +3,44 @@
  */
 package qa;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 /**
  * @author Deepak
  *
  */
 public class InformationExtraction {
-	public static String extractKeywords(String sentence)
+	
+	
+	private static String p= "[?(){}\\[\\]\\/]" ; 
+	
+	
+	public static String extractKeywords(String question)
 	{
-		return null;
+		 String keywordsString = "";
+		 question=question.replaceAll(p, " ");
+			
+		 MaxentTagger tagger =  new MaxentTagger("External Lib/taggers/english-left3words-distsim.tagger");
+		 String taggedQues=tagger.tagString(question);
+		 String [] quesTags= taggedQues.split("_|\\s");
+		
+		String start="\"";
+		int startIndex=question.indexOf(start);
+		int lastIndex=question.indexOf(start);
+		//keyWords.add(question.substring(startIndex, lastIndex));
+		
+			
+		for (int i=1;i<quesTags.length;i++){
+			if(quesTags[i].equals("NN")||quesTags[i].equals("NNP")||quesTags[i].equals("JJ")||quesTags[i].equals("VB")){
+
+			keywordsString = keywordsString + quesTags[i-1] + question.substring(startIndex, lastIndex)+" ";
+			
+			}
+		
 	}
+		return keywordsString;
+		}
 
 }
